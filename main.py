458b5fa385bdfae579dc1c7c26612606568bd625
greenstealth <160681164+greenstealth@users.noreply.github.com>
2024-03-20 00:15:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,28 +14,17 @@ def print_banner():
     print(f"\033[92m{banner}\033[0m")  # 92 é o código para a cor verde no terminal
 
 def main_menu():
-    print("1. Raspagem de e-mails brasileiros usando Google Dorks")
-    print("2. Raspagem de e-mails do Gmail usando Google Dorks")
-    choice = input("Escolha uma opção: ")
-    return choice
+    print("Insira o termo de busca para a raspagem de e-mails usando Google Dorks.")
+    print("Exemplo: 'site:br contato@', 'site:com gmail.com'")
+    query = input("Digite o termo de busca: ")
+    return query
 
 def main():
     print_banner()
-    choice = main_menu()
+    query = main_menu()
     output_file = "emails.csv"
-
-    if choice == '1':
-        query = 'site:br "contato@"'
-        scraper = EmailScraper(query)
-        scraper.run(output_file)
-        emails = scraper.scrape_emails_from_url(query)  # Esta linha não é mais necessária
-    elif choice == '2':
-        query = 'site:com "gmail.com"'
-        scraper = EmailScraper(query)
-        scraper.run(output_file)
-        emails = scraper.scrape_emails_from_url(query)  # Esta linha não é mais necessária
-
-    # Aqui, você poderia adicionar a lógica para filtrar e salvar os e-mails, se necessário.
+    scraper = EmailScraper(query)
+    scraper.run(output_file)
 
 if __name__ == "__main__":
     main()
