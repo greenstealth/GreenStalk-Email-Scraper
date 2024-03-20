67292b5d7fde94b06c50c172e984c2e707eb9b16
greenstealth <160681164+greenstealth@users.noreply.github.com>
2024-03-19 23:57:10 -0300

    Update email_saver.py

--- email_saver.py
+++ email_saver.py
@@ -1,8 +1,14 @@
 import csv
+import os
 
 def save_emails_to_csv(emails, filename):
-    with open(filename, mode='w', newline='') as file:
+    mode = 'a' if os.path.exists(filename) else 'w'
+    # Remover duplicatas convertendo a lista para um conjunto
+    unique_emails = set(emails)
+
+    with open(filename, mode=mode, newline='') as file:
         writer = csv.writer(file)
-        for email in emails:
+        for email in sorted(unique_emails):  # Ordenar os e-mails alfabeticamente
             writer.writerow([email])
+
     print(f"E-mails salvos com sucesso em {filename}")
